e08e601827e6b6beddb358357b0058eb16e855e2
KristeriX99 <80916606+KristeriX99@users.noreply.github.com>
2021-03-19 09:54:43 +0200

    Update Grunts.js

--- src/components/Grunts.js
+++ src/components/Grunts.js
@@ -4,7 +4,13 @@ const Grunts = () => {
     return (
        <div>
           <h1>Gruntsmakšķerēšana</h1>
-          <p>Teksts par gruntsmakskeresanu</p>
+          <p>Būtībā gruntsmakšķerēšana ietver svērtā āķa vai mānekļa nolaišanu līdz ūdenstipnes dibenam. Makšķerēšana ar gruntsmakšķeri ir visai pasīvs makšķerēšanas veids līdz brīdim, kad sāk intensīvi ķerties. Visbiežāk ar gruntsmakšķeri makšķerē vimbas, vēdzeles, zušus, samus, butes, zandartus, plaužus. 
+Viena no lietām, kas grunts makšķerēšanu padara tik aizrojošu, ir fakts, ka jūs to varat praktizēt bezgalīgi daudzos veidos. Atkarībā no ūdens, kuru makšķerējat, un no tā, ko vēlaties noķert, varat izvēlēties no vairākiem makšķerēšanas stiliem.
+Gruntsmakšķerēšanu var veikt no laivas un arī no sauszemes, izmantojot vai nu rokas, vai makšķeres. <br></br> 
+Šīs metodes galvenais mērķis ir nogādāt ēsmu ūdens dibenā un vilinošā veidā pasniegt to zivīm un ievilināt tās āķī. Apakšējā makšķerēšanā auklu vada smags svina grāvējs, kam seko ēsma un āķis. Kad zivis paņems ēsmu, tā sajutīs pretestību no svara, kas ir pakārts zemāk, un tas, savukārt, aizbīdīs, savukārt viņus saķers ar āķi, kas karājas ar ēsmu. Smago grimstnieku izmanto, lai palīdzētu izmest ēsmu un dziļi ūdens dzelmē. Svina svars arī saglabās āķi un ēsmu, neraugoties uz spēcīgu straumi un augstiem viļņiem. Tas makšķerniekam dos labas priekšrocības.
+</p>
+      <img src="gruntmakšķerēšana.jfif"></img>
+      <img src="gruntmakšķerēšana2.jfif"></img>
        </div>
     );
 }
