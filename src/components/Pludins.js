9941cc3f870640377e45cd369be872c36a183ce5
KristeriX99 <80916606+KristeriX99@users.noreply.github.com>
2021-03-19 09:55:31 +0200

    Update Pludins.js

--- src/components/Pludins.js
+++ src/components/Pludins.js
@@ -4,7 +4,14 @@ const Pludins = () => {
     return (
        <div>
           <h1>Pludiņmakšķerēšana</h1>
-          <p>Teksts par pludinmakskeresanu</p>
+          <p>Pludiņmakšķerēšana ir metode, ar kuru sāk gandrīz visi makšķernieki. Pārsvarā tiek izmantota miermīlīgu zivju ķeršanai. Ir vajadzīga makšķere, aukla un pludiņš, kas signalizēs brīdi, kad zivs iekāro piedāvāto ēsmu. Par ēsmu izmanto gan dabīgus mānekļus, gan mākslīgus.
+<br></br>
+Pludiņmakšķerēšanas galvenais noslēpums ir pievērst uzmanību sīkākām detaļām. Atcerieties, ka zivis ne vienmēr ir gatavas vilkt pludiņu zem ūdens, un, ja rodas pārāk liela pretestība, daudzi vienkārši ļaus spolei atbrīvoties. Lai iegūtu labākos rezultātus, iemērciet galu pēc iespējas zemāk.
+Pludiņmakšķerēšana prasīs laiku lai iemācītos, taču, tiklīdz esat noķēris kādu zivi, pieredze un vajadzīgās prasmes atnāks ātri. Tas var būt ļoti aizraujošs laika pavadīšanas veids.
+</p>
+<img src="How-to-Drift-Fish-Float-Setup-1.jpg"></img>
+<img src="download.jfif"></img>
+
        </div>
     );
 }
